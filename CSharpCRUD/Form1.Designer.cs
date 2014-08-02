353f1b642a56da71a8bad7c8687198a7c0d20296
Alleo Indong <freedom_war550@live.com.ph>
2014-08-02 17:04:22 +0800

    First Commit

---
+++ CSharpCRUD/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace CSharpCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.txtBday = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 139);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(787, 279);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            this.columnHeader1.Width = 91;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "First Name";
+            this.columnHeader2.Width = 248;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Last Name";
+            this.columnHeader3.Width = 242;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Middle Name";
+            this.columnHeader4.Width = 248;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Birthday";
+            this.columnHeader5.Width = 202;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(161, 27);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(251, 26);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(161, 59);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(251, 26);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiddleName.Location = new System.Drawing.Point(161, 91);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(251, 26);
+            this.txtMiddleName.TabIndex = 5;
+            // 
+            // txtBday
+            // 
+            this.txtBday.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBday.Location = new System.Drawing.Point(443, 27);
+            this.txtBday.Name = "txtBday";
+            this.txtBday.Size = new System.Drawing.Size(251, 26);
+            this.txtBday.TabIndex = 6;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(161, 29);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox1.Text = "Delete Item";
+            this.toolStripTextBox1.Click += new System.EventHandler(this.toolStripTextBox1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(432, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 41);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(563, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 41);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 430);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtBday);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.TextBox txtBday;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ CSharpCRUD/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace CSharpCRUD
+{
+    public partial class Form1 : Form
+    {
+
+        // Connectionstring that will connect mysql and .net
+        private string connectionString = "Server=127.0.0.1;Database=seminar;Uid=root;Pwd=;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This will handle the loading of the form
+        /// </summary>
+        /// <param name="sender">The object sender</param>
+        /// <param name="e">Event Handler</param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RetrieveContactData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        void RetrieveContactData()
+        {
+            // 1. Create the connection object
+            using (MySqlConnection dbConnection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    // 1.a Open the connection
+                    dbConnection.Open();
+
+                    // 2. Build the command query
+                    string query = "SELECT * FROM contact";
+
+                    // Create sql command object
+                    MySqlCommand sqlCmd = new MySqlCommand(query, dbConnection);
+
+                    // Create a reader for the raw data
+                    MySqlDataReader reader = sqlCmd.ExecuteReader();
+
+                    // Clear all the data from the listview first
+                    // before loading the data from the database
+                    listView1.Items.Clear();
+
+                    while (reader.Read())
+                    {
+                        // put the read data on the listview
+                        listView1.Items.Add(reader["id"].ToString());
+
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(reader["firstName"].ToString());
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(reader["lastName"].ToString());
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(reader["middleName"].ToString());
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(reader["bday"].ToString());
+                    }
+
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+
+                // 4. Display the error
+            }
+        }
+
+        void DeleteContactData()
+        {
+            // 1. Create the connection object
+            using (MySqlConnection dbConnection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    // 1.a Open the connection
+                    dbConnection.Open();
+
+                    // 2. Build the command query
+                    string query = "DELETE FROM contact WHERE id = @id";
+
+                    // Create sql command object
+                    MySqlCommand sqlCmd = new MySqlCommand(query, dbConnection);
+
+                    sqlCmd.Parameters.AddWithValue("id", listView1.SelectedItems[0].Text);
+
+                    // Execute the Insert Command
+                    sqlCmd.ExecuteNonQuery();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+
+                // 4. Display the error
+            }
+
+            RetrieveContactData();
+        }
+
+        void UpdateContactDate()
+        {
+            // 1. Create the connection object
+            using (MySqlConnection dbConnection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    // 1.a Open the connection
+                    dbConnection.Open();
+
+                    // 2. Build the command query
+                    string query = "UPDATE contact SET firstName = @firstName, lastName = @lastName, middleName = @middleName, bday = @bday WHERE id = @id";
+
+
+                    // Create sql command object
+                    MySqlCommand sqlCmd = new MySqlCommand(query, dbConnection);
+
+                    sqlCmd.Parameters.AddWithValue("firstName", txtFirstName.Text);
+                    sqlCmd.Parameters.AddWithValue("lastName", txtLastName.Text);
+                    sqlCmd.Parameters.AddWithValue("middleName", txtMiddleName.Text);
+                    sqlCmd.Parameters.AddWithValue("bday", txtBday.Text);
+                    sqlCmd.Parameters.AddWithValue("id", listView1.SelectedItems[0].Text);
+
+                    // Execute the Insert Command
+                    sqlCmd.ExecuteNonQuery();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+
+                // 4. Display the error
+            }
+
+            RetrieveContactData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 1. Create the connection object
+            using (MySqlConnection dbConnection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    // 1.a Open the connection
+                    dbConnection.Open();
+
+                    // 2. Build the command query
+                    string query = "INSERT INTO contact(firstName, lastName, middleName, bday) VALUES(@firstName, @lastName, @middleName, @bday)";
+
+                    // Create sql command object
+                    MySqlCommand sqlCmd = new MySqlCommand(query, dbConnection);
+
+                    sqlCmd.Parameters.AddWithValue("firstName", txtFirstName.Text);
+                    sqlCmd.Parameters.AddWithValue("lastName", txtLastName.Text);
+                    sqlCmd.Parameters.AddWithValue("middleName", txtMiddleName.Text);
+                    sqlCmd.Parameters.AddWithValue("bday", txtBday.Text);
+
+                    // Execute the Insert Command
+                    sqlCmd.ExecuteNonQuery();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+
+                // 4. Display the error
+            }
+
+            RetrieveContactData();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+            DeleteContactData();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DeleteContactData();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            UpdateContactDate();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            // first name
+            txtFirstName.Text = listView1.SelectedItems[0].SubItems[1].Text;
+
+            // last name
+            txtLastName.Text = listView1.SelectedItems[0].SubItems[2].Text;
+
+            // middle name
+            txtMiddleName.Text = listView1.SelectedItems[0].SubItems[3].Text;
+
+            // bday
+            txtBday.Text = listView1.SelectedItems[0].SubItems[4].Text; 
+        }
+    }
+}
